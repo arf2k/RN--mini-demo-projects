c761faaf2d481cc27672318a165e07f62dd5e620
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-17 17:32:23 -0400

    Add a couple comp

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,2 +1,23 @@
 import React from "react";
-import { Text, Stylesheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+
+const ComponentsScreen = () => {
+     const name = "Alex"
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting Started with React Native!</Text>
+      <Text style={styles.subHeader}>My name is {name}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 45,
+  },
+  subHeader: {
+       fontSize: 20,
+  }
+});
+
+export default ComponentsScreen;
