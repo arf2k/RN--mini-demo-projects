e63344ee5a4c221a3c091d9d154af8a1e603895e
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-18 16:44:01 -0400

    Update expo cli and add button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Xander's Phone</Text>;
+  return (
+  <View>
+  <Text style={styles.text}>Xander's Phone</Text>
+  <Button title="Go to Components Demo"
+  onPress={() => console.log("button")}
+  />
+  </View>
+  )
 };
 
 const styles = StyleSheet.create({
